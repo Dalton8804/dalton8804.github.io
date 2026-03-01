622534abc0ed5c5edd0bfbbeba0dad67a63f8e98
Dalton Avery <daltonjavery@gmail.com>
2026-03-01 17:37:00 -0600

    triple typo

--- app/books/quotes/page.tsx
+++ app/books/quotes/page.tsx
@@ -4,7 +4,7 @@ export default function Quotes() {
     return (
         <PageShell title="quotes">
             <blockquote>The child who refuses to travel in the father's harness, this is the symbol of man's most unique capability. "I do not have to be what my father was. I do not have to obey my father's rules or even believe everything he believed. It is my strength as a human that I can make my own choices of what to believe and what not to believe, of what to be and what not to be. - Frank Herbert, Children of Dune</blockquote>
-            <blockquote>[...] they are most self-limiting. Then know this somehwere in the damps of ther deepest consciousness but cannot spare the time to deal with that or anything else except the immediate scramble for survival. - Frank Herbert, Heretics of Dune</blockquote>
+            <blockquote>[...] they are most self-limiting. They know this somewhere in the damps of their deepest consciousness but cannot spare the time to deal with that or anything else except the immediate scramble for survival. - Frank Herbert, Heretics of Dune</blockquote>
             <blockquote>listening never made any man dumber - M.L. Wang, Sword of Kaigen</blockquote>
         </PageShell>
     );
