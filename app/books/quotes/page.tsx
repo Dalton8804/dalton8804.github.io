01ab7dae460d2ee98dfe540a5928f0e003422be9
Dalton Avery <daltonjavery@gmail.com>
2026-03-10 20:33:25 -0500

    add blogroll

--- app/books/quotes/page.tsx
+++ app/books/quotes/page.tsx
@@ -1,8 +1,10 @@
 import PageShell from "@/components/PageShell";
+import Link from "next/link";
 
 export default function Quotes() {
     return (
         <PageShell title="quotes">
+            <h4><Link href="/books">&lt; back to books</Link></h4>
             <blockquote>The child who refuses to travel in the father's harness, this is the symbol of man's most unique capability. "I do not have to be what my father was. I do not have to obey my father's rules or even believe everything he believed. It is my strength as a human that I can make my own choices of what to believe and what not to believe, of what to be and what not to be. - Frank Herbert, Children of Dune</blockquote>
             <blockquote>[...] they are most self-limiting. They know this somewhere in the damps of their deepest consciousness but cannot spare the time to deal with that or anything else except the immediate scramble for survival. - Frank Herbert, Heretics of Dune</blockquote>
             <blockquote>listening never made any man dumber - M.L. Wang, Sword of Kaigen</blockquote>
